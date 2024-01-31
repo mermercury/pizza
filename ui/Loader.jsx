f8baf98ee6eb05d7632322cfbb23fd543b93ef4a
mermercury <97878411+mermercury@users.noreply.github.com>
2024-01-31 11:48:17 +0800

    Delete ui directory

--- ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loader() {
-  return (
-    <div className='absolute bg-slate-200/20 backdrop-blur-sm inset-0 flex items-center justify-center'>
-      <div className='loader'></div>
-    </div>
-  )
-}
