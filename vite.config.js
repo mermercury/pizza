0f3d075f1ed97ce81d2563b9292d92a31df70599
mermercury <97878411+mermercury@users.noreply.github.com>
2024-02-04 23:50:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { compression } from 'vite-plugin-compression2';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +14,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), compression()],
+  plugins: [react()],
 });
